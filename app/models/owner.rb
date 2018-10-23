64ad9584e4efcb47cc0950755dfb08106491f220
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-10-22 22:36:53 -0400

    validation on owner's model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,7 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
+  validates :first_name, presence: true, length: {maximum: 255}
+  validates :last_name, presense: true, length: {maximum: 255}
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
 
   before_save :normalize_phone_number
 
